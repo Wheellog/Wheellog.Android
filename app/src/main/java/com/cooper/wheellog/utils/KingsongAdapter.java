d5835347e47de389a97284eb2dbe5d0ae89e9b0f
paymicro <hronos2t@gmail.com>
2020-08-24 14:06:45 +0300

    - add IWheelAdapter

---
+++ app/src/main/java/com/cooper/wheellog/utils/KingsongAdapter.java
@@ -0,0 +1,23 @@
+package com.cooper.wheellog.utils;
+
+public class KingsongAdapter implements IWheelAdapter {
+    @Override
+    public boolean decode(byte[] data) {
+        return false;
+    }
+
+    @Override
+    public void updatePedalsMode(int pedalsMode) {
+
+    }
+
+    @Override
+    public void updateLightMode(int lightMode) {
+
+    }
+
+    @Override
+    public void updateMaxSpeed(int wheelMaxSpeed) {
+
+    }
+}
