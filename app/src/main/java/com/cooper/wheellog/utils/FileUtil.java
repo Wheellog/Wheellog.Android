4920d52f2d80d2bf8794ba51021ba14aa9c4642d
paymicro <hronos2t@gmail.com>
2020-08-26 22:52:47 +0300

    fix file path

--- app/src/main/java/com/cooper/wheellog/utils/FileUtil.java
+++ app/src/main/java/com/cooper/wheellog/utils/FileUtil.java
@@ -3,13 +3,16 @@ package com.cooper.wheellog.utils;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 
 import androidx.annotation.RequiresApi;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -28,7 +31,6 @@ public class FileUtil {
     private Context context;
     private File file;
     private Uri uri;
-    private String pathName = "WheelLog Logs";
     private Dictionary<String, Uri> AndroidQCache;
     private boolean ignoreTimber = false;
 
@@ -45,68 +47,16 @@ public class FileUtil {
         return file;
     }
 
-    public Uri getUri() {
-        return uri;
-    }
-
     public String getAbsolutePath() {
-        return uri != null ? uri.getPath() : file.getAbsolutePath();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    private String getExternalStoreName() {
-        Set<String> names = MediaStore.getExternalVolumeNames(context);
-        for (String n : names) {
-            if (n.contains("external")) {
-                return n;
-            }
+        if (isNull()) {
+            return null;
         }
-        return names.toArray()[0].toString();
+
+        return file.getAbsolutePath();
     }
 
     public boolean isNull() {
         return file == null || file.toString().equals("null");
-    }
-
-    private ContentResolver getContentResolver() {
-        if (context != null) {
-            return context.getContentResolver();
-        } else {
-            return null;
-        }
-    }
-
-    public boolean isFileExists(final String filePath) {
-        if (isNull()) {
-            return false;
-        }
-        if (Build.VERSION.SDK_INT >= 29) {
-            try {
-                Uri uri = Uri.parse(filePath);
-                ContentResolver resolver = getContentResolver();
-                AssetFileDescriptor afd = resolver.openAssetFileDescriptor(uri, "r");
-                if (afd == null) return false;
-                try {
-                    afd.close();
-                } catch (IOException ignore) {
-                }
-            } catch (FileNotFoundException e) {
-                return false;
-            }
-            return true;
-        } else {
-            return file.exists();
-        }
-    }
-
-    private String getMimeType(String fileName) {
-        if (fileName.endsWith(".csv")) {
-            return "text/csv";
-        } else if (fileName.endsWith(".html") || fileName.endsWith(".htm")) {
-            return "text/html";
-        } else {
-            return "*/*";
-        }
     }
 
     public boolean prepareFile(String fileName) {
@@ -118,7 +68,7 @@ public class FileUtil {
             // It need to not create dublicate files
             uri = AndroidQCache.get(fileName);
             if (uri != null) {
-                file = new File(String.valueOf(uri));
+                file = new File(getPathFromUri(uri));
                 return true;
             }
 
@@ -126,11 +76,11 @@ public class FileUtil {
             contentValues.put(MediaStore.Downloads.DISPLAY_NAME, fileName);
             contentValues.put(MediaStore.Downloads.TITLE, fileName);
             contentValues.put(MediaStore.Downloads.MIME_TYPE, getMimeType(fileName));
-            contentValues.put(MediaStore.Downloads.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS + File.separator + pathName);
+            contentValues.put(MediaStore.Downloads.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS + File.separator + Constants.LOG_FOLDER_NAME);
             Uri contentUri = MediaStore.Downloads.getContentUri(getExternalStoreName());
 
             uri = getContentResolver().insert(contentUri, contentValues);
-            file = new File(String.valueOf(uri));
+            file = new File(getPathFromUri(uri));
             AndroidQCache.put(fileName, uri);
         } else {
             // api 28 or less
@@ -183,5 +133,75 @@ public class FileUtil {
             return false;
         }
         return true;
+    }
+
+    @NotNull
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private String getExternalStoreName() {
+        Set<String> names = MediaStore.getExternalVolumeNames(context);
+        for (String n : names) {
+            if (n.contains("external")) {
+                return n;
+            }
+        }
+        return names.toArray()[0].toString();
+    }
+
+    @Nullable
+    private ContentResolver getContentResolver() {
+        if (context != null) {
+            return context.getContentResolver();
+        } else {
+            return null;
+        }
+    }
+
+    @NotNull
+    private String getMimeType(@NotNull String fileName) {
+        if (fileName.endsWith(".csv")) {
+            return "text/csv";
+        } else if (fileName.endsWith(".html") || fileName.endsWith(".htm")) {
+            return "text/html";
+        } else {
+            return "*/*";
+        }
+    }
+
+    @Nullable
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String getPathFromUri(@Nullable Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn();
+        }
+        if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    @Nullable
+    private String getDataColumn() {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, null, null,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
     }
 }
