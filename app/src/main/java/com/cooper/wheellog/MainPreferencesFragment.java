a4c737306d654cecfc4545c086cd1330800c4807
palachzzz <7zzzzzzz@mail.ru>
2020-10-22 23:02:05 +0300

    What about about box? (#50)
    * what about about box?
    
    * smallfix
    
    Co-authored-by: palachzzz <palachzzz.wl@gmail.com>

--- app/src/main/java/com/cooper/wheellog/MainPreferencesFragment.java
+++ app/src/main/java/com/cooper/wheellog/MainPreferencesFragment.java
@@ -321,7 +321,7 @@ public class MainPreferencesFragment extends PreferenceFragmentCompat implements
                             String buildTime = BuildConfig.BUILD_TIME;
                             new AlertDialog.Builder(activity)
                                     .setTitle(R.string.about_app_title)
-                                    .setMessage(Html.fromHtml(String.format("Version %s <br>build at %s <br>by <i>Palachzzz</i> <br><a href=\"palachzzz.wl@gmail.com\">palachzzz.wl@gmail.com</a> <br> Thanks to:<br>JumpMaster - project initiator<br>cedbossneo - Inmotion support<br>juliomap - Tizen support<br>MacPara - some improvements<br>datarsoja - KS alerts<br>and others!", versionName, buildTime)))
+                                    .setMessage(Html.fromHtml(String.format("Version %s <br>build at %s <br><a href=\"https://github.com/Wheellog/Wheellog.Android/\">github.com/Wheellog/Wheellog.Android</a> <br> Thanks to all <a href=\"https://github.com/Wheellog/Wheellog.Android/contributors\">contributors</a>", versionName, buildTime)))
                                     .setPositiveButton(android.R.string.ok, (dialog, which) -> { })
                                     .setIcon(android.R.drawable.ic_dialog_info)
                                     .show();
