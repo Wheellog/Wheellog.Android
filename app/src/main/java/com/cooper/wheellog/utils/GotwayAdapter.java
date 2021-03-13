eaae3d6f777882c30d0c0176341ad7970e23ac3d
Pavel Yakovlev <hronos2t@gmail.com>
2021-03-14 02:06:04 +0300

    in gotway

--- app/src/main/java/com/cooper/wheellog/utils/GotwayAdapter.java
+++ app/src/main/java/com/cooper/wheellog/utils/GotwayAdapter.java
@@ -195,40 +195,16 @@ public class GotwayAdapter extends BaseAdapter {
         final byte[] hhh = new byte[1];
         final byte[] lll = new byte[1];
         if (maxSpeed != 0) {
-            int wheelMaxSpeed2 = maxSpeed;
-            hhh[0] = (byte)((wheelMaxSpeed2/10)+0x30);
-            lll[0] = (byte)((wheelMaxSpeed2%10)+0x30);
-            WheelData.getInstance().getBluetoothLeService().writeBluetoothGattCharacteristic("b".getBytes());
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    sendCommand("W", "Y");
-                }
-            }, 100);
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    sendCommand(hhh, lll, 100);
-                }
-            }, 300);
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    sendCommand("b","b");
-                }
-            }, 500);
-
+            hhh[0] = (byte) ((maxSpeed / 10) + 0x30);
+            lll[0] = (byte) ((maxSpeed % 10) + 0x30);
+            WheelData.getInstance().bluetoothCmd("b".getBytes());
+            new Handler().postDelayed(() -> sendCommand("W", "Y"), 100);
+            new Handler().postDelayed(() -> sendCommand(hhh, lll, 100), 300);
+            new Handler().postDelayed(() -> sendCommand("b", "b"), 500);
         } else {
             sendCommand("b", "\"");
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    sendCommand("b", "b");
-                }
-            }, 200);
-
+            new Handler().postDelayed(() -> sendCommand("b", "b"), 200);
         }
-
     }
 
     static class gotwayUnpacker {
