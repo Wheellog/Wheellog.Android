7181a52a3151d4e4a1a4f79164585c6af804b056
Pavel Yakovlev <hronos2t@gmail.com>
2021-04-09 13:33:15 +0300

    in tests

--- app/src/test/java/com/cooper/wheellog/utils/GotwayVirtualAdapterTest.kt
+++ app/src/test/java/com/cooper/wheellog/utils/GotwayVirtualAdapterTest.kt
@@ -38,7 +38,7 @@ class GotwayVirtualAdapterTest {
         val byteArray1 = "55AA19C1000000000000008CF0000001FFF80018".hexToByteArray()
 
         // Act.
-        val result1 = adapter.decode(byteArray1)
+        adapter.decode(byteArray1)
 
         // Assert.
         assertThat(data.wheelType).isEqualTo(Constants.WHEEL_TYPE.GOTWAY)
@@ -51,7 +51,7 @@ class GotwayVirtualAdapterTest {
         val byteArray1 = "DC5A5C20238A0112121A00004D450005064611F2".hexToByteArray()
 
         // Act.
-        val result1 = adapter.decode(byteArray1)
+        adapter.decode(byteArray1)
 
         // Assert.
         assertThat(data.wheelType).isEqualTo(Constants.WHEEL_TYPE.VETERAN)
