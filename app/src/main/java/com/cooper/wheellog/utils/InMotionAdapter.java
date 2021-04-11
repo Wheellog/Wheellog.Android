634c33c173621fdabea75f0dc754458c66e132fc
Pavel Yakovlev <hronos2t@gmail.com>
2021-04-11 23:14:16 +0300

    fixes verification inmotion packet when it ends A5 byte

--- app/src/main/java/com/cooper/wheellog/utils/InMotionAdapter.java
+++ app/src/main/java/com/cooper/wheellog/utils/InMotionAdapter.java
@@ -810,13 +810,14 @@ public class InMotionAdapter extends BaseAdapter {
                 return null;  // Header and tail not correct
             }
             Timber.i("Before escape %s", StringUtil.toHexString(buffer));
-            byte[] dataBuffer = Arrays.copyOfRange(buffer, 2, buffer.length - 3);
+            int len = buffer.length - 3;
+            byte[] dataBuffer = Arrays.copyOfRange(buffer, 2, len);
 
             dataBuffer = CANMessage.unescape(dataBuffer);
             Timber.i("After escape %s", StringUtil.toHexString(dataBuffer));
             byte check = CANMessage.computeCheck(dataBuffer);
 
-            byte bufferCheck = buffer[buffer.length - 3];
+            byte bufferCheck = buffer[len];
             if (check == bufferCheck) {
                 Timber.i("Check OK");
             } else {
@@ -1236,6 +1237,7 @@ public class InMotionAdapter extends BaseAdapter {
 
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         int oldc = 0;
+        int old2c = 0;
         UnpackerState state = UnpackerState.unknown;
 
         byte[] getBuffer() {
@@ -1245,7 +1247,7 @@ public class InMotionAdapter extends BaseAdapter {
         boolean addChar(int c) {
             if (state == UnpackerState.collecting) {
                 buffer.write(c);
-                if (c == (byte) 0x55 && oldc == (byte) 0x55) {
+                if (c == (byte) 0x55 && oldc == (byte) 0x55 && old2c != (byte) 0xA5) {
                     state = UnpackerState.done;
                     updateStep = 0;
                     oldc = 0;
@@ -1260,6 +1262,7 @@ public class InMotionAdapter extends BaseAdapter {
                     state = UnpackerState.collecting;
                 }
             }
+            old2c = oldc;
             oldc = c;
             return false;
         }
