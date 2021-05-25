27c1aa0ee9c7ca41ce377af71e34b622b87d4484
ggoraa <ggoraa1029@gmail.com>
2021-05-25 22:07:26 +0300

    Added viewmodel class for wheelview

---
+++ app/src/main/java/com/cooper/wheellog/views/viewmodels/WheelViewModel.kt
@@ -0,0 +1,9 @@
+package com.cooper.wheellog.views.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class WheelViewModel : ViewModel() {
+    
+}
