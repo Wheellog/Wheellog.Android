9426e9ea8bb824567ef853ba32ab977761ba8353
ggoraa <ggoraa1029@gmail.com>
2021-05-26 20:23:38 +0300

    Removed WheelViewModel.kt

--- app/src/main/java/com/cooper/wheellog/views/viewmodels/WheelViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.cooper.wheellog.views.viewmodels
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class WheelViewModel : ViewModel() {
-    
-}
