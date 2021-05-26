977c401e2e583c59b57e5c7f163215f34788f575
ggoraa <ggoraa1029@gmail.com>
2021-05-26 20:21:32 +0300

    Added a function to update WheelView

--- app/src/main/java/com/cooper/wheellog/views/WheelView.kt
+++ app/src/main/java/com/cooper/wheellog/views/WheelView.kt
@@ -1,11 +1,11 @@
 package com.cooper.wheellog.views
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -14,41 +14,38 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.AbstractComposeView
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.cooper.wheellog.R
 import com.cooper.wheellog.WheelData
 import com.cooper.wheellog.WheelLog
 import com.cooper.wheellog.utils.SomeUtil.Companion.getColorEx
 
-@SuppressLint("ClickableViewAccessibility")
 class WheelView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet?,
     defStyleAttr: Int = 0
 ) : AbstractComposeView(context, attrs, defStyleAttr) {
-    var mSpeedDouble = WheelData.getInstance().speedDouble
+    var mSpeedDouble = mutableStateOf(WheelData.getInstance().speedDouble)
+
 
     @Composable
     override fun Content() {
-        val speed = remember { mutableStateOf(mSpeedDouble) }
-        val batteryLevel = remember { mutableStateOf(WheelData.getInstance().batteryLevel) }
-        val temperature = remember { mutableStateOf(WheelData.getInstance().temperature) }
+        val speed = remember { mSpeedDouble }
+        val batteryLevel = remember { mutableStateOf(WheelData.getInstance().batteryLevel.toFloat()) }
+        val temperature = remember { mutableStateOf(WheelData.getInstance().temperature.toFloat()) }
         DataArcs(
             modifier = Modifier.fillMaxWidth(),
-            mainArc = speed.value.toFloat(),
-            leftArc = batteryLevel.value.toFloat(),
-            rightArc = temperature.value.toFloat()
+            mainArc = speed,
+            leftArc = batteryLevel,
+            rightArc = temperature
         )
     }
 
     @Composable
     private fun DataArcs(
         modifier: Modifier,
-        mainArc: Float,
-        leftArc: Float,
-        rightArc: Float
+        mainArc: MutableState<Double>,
+        leftArc: MutableState<Float>,
+        rightArc: MutableState<Float>,
     ) {
         Canvas(modifier) {
             /*
@@ -60,7 +57,7 @@ class WheelView @JvmOverloads constructor(
             val mainArcStroke = Stroke(width = mainArcSize / 8)
 
             // Calculation of arc sizes
-            val mainArcSweepAngle = 252f * (mainArc / WheelLog.AppConfig.maxSpeed)
+            val mainArcSweepAngle = 252f * (mainArc.value / WheelLog.AppConfig.maxSpeed)
 
             // Background
             drawArc(
@@ -72,22 +69,30 @@ class WheelView @JvmOverloads constructor(
                 size = Size(mainArcSize - mainArcStroke.width, mainArcSize - mainArcStroke.width),
                 style = mainArcStroke
             )
-            // Foreground
+            // Foreground content
             drawArc(
                 color = Color(getColorEx(R.color.wheelview_main_positive_dial)),
                 144f,
-                mainArcSweepAngle,
+                mainArcSweepAngle.toFloat(),
                 false,
                 topLeft = Offset(mainArcStroke.width / 2, mainArcStroke.width / 2),
                 size = Size(mainArcSize - mainArcStroke.width, mainArcSize - mainArcStroke.width),
                 style = mainArcStroke
             )
+            // And foreground marks
         }
     }
 
     @Composable
     private fun InfoBlock(label: String, value: String) {
 
+    }
+
+    // Later this function will be replaced by ViewModel
+    // and Kotlin Flow, so this view will update by itself,
+    // without external update calls
+    fun refresh() {
+        mSpeedDouble.value = WheelData.getInstance().speedDouble
     }
 
     fun resetBatteryLowest() {
