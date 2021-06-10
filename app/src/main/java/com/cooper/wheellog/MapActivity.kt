b31a0b007beb6c2d24901b7998234c1cc94f1855
Pavel Yakovlev <hronos2t@gmail.com>
2021-06-11 02:10:49 +0300

    - legend - optimization - accent color

--- app/src/main/java/com/cooper/wheellog/MapActivity.kt
+++ app/src/main/java/com/cooper/wheellog/MapActivity.kt
@@ -1,6 +1,5 @@
 package com.cooper.wheellog
 
-import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -8,19 +7,124 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.preference.PreferenceManager
 import com.cooper.wheellog.utils.MathsUtil
+import com.cooper.wheellog.utils.SomeUtil.Companion.getColorEx
 import kotlinx.coroutines.*
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Polyline
+import org.osmdroid.views.overlay.ScaleBarOverlay
 import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
 import timber.log.Timber
 import java.io.*
 
 
 class MapActivity : AppCompatActivity() {
+    private val latitudeHeader = "latitude"
+    private val longitudeHeader = "longitude"
+    private val altHeader = "gps_alt"
+    private val gpsSpeedHeader = "gps_speed"
+    private val distanceHeader = "distance"
+    private val batteryHeader = "battery_level"
     private lateinit var map: MapView
+
+    private fun parseFileToPolyLine(extras: Bundle): Polyline? {
+        val inputStream: InputStream?
+        try {
+            inputStream = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                // Android 9 or less
+                FileInputStream(File(extras.get("path") as String))
+            } else {
+                // Android 10+
+                applicationContext.contentResolver.openInputStream(extras.get("uri") as Uri)
+            }
+        } catch (ex: Exception) {
+            Timber.wtf(ex.localizedMessage)
+            return null
+        }
+        if (inputStream == null) {
+            Timber.wtf("Failed to create inputStream for %s", extras.get("title"))
+            return null
+        }
+
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val header = reader.readLine().split(",").toTypedArray()
+        val latIndex = header.indexOf(latitudeHeader)
+        val longIndex = header.indexOf(longitudeHeader)
+        val altIndex = header.indexOf(altHeader)
+        if (latIndex == -1 || longIndex == -1) {
+            inputStream.close()
+            Timber.wtf("%s file does not contain geolocation data.", extras.get("title"))
+            return null
+        }
+
+        // for statistics
+        var latitude = 0.0
+        var longitude = 0.0
+        val gpsSpeedIndex = header.indexOf(gpsSpeedHeader)
+        val distanceIndex = header.indexOf(distanceHeader)
+        val batteryIndex = header.indexOf(batteryHeader)
+        var maxSpeed = 0.0
+        var distance = 0
+        var startBattery = 0
+        var endBattery = 0
+
+        val geoPoints = ArrayList<GeoPoint>()
+        try {
+            var i = 0
+            reader.forEachLine { line ->
+                val row = line.split(",").toTypedArray()
+                val latitudeNew = row[latIndex].toDouble()
+                val longitudeNew = row[longIndex].toDouble()
+                if (latitudeNew != latitude && longitudeNew != longitude) {
+                    latitude = latitudeNew
+                    longitude = longitudeNew
+                    val altitude = row[altIndex].toDouble()
+                    geoPoints.add(i++, GeoPoint(latitude, longitude, altitude))
+
+                    // stats
+                    val speed = row[gpsSpeedIndex].toDouble()
+                    maxSpeed = maxSpeed.coerceAtLeast(speed)
+                    distance = row[distanceIndex].toInt()
+                    val batteryLevel = row[batteryIndex].toInt()
+                    if (i == 1) {
+                        startBattery = batteryLevel
+                    }
+                    endBattery = batteryLevel
+                }
+            }
+        } catch (ex: Exception) {
+            Timber.wtf(ex.localizedMessage)
+            return null
+        } finally {
+            inputStream.close()
+        }
+
+        val legend =
+            try {
+                String.format(
+                    "%s\nDistance: %.2f km\nMax GPS speed: %.2f\nBattery: %d%% > %d%%",
+                    extras.get("title"),
+                    distance / 1000.0,
+                    maxSpeed,
+                    startBattery,
+                    endBattery
+                )
+            } catch (ex: Exception) {
+                ""
+            }
+
+        return Polyline(map, true).apply {
+            setPoints(geoPoints)
+            title = legend
+            outlinePaint.apply {
+                color = applicationContext.getColorEx(R.color.accent)
+                isAntiAlias = true
+                strokeWidth = 13f
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,16 +137,15 @@ class MapActivity : AppCompatActivity() {
         setContentView(R.layout.activity_map)
 
         map = findViewById(R.id.mapView)
-        val rotationGestureOverlay = RotationGestureOverlay(map).apply {
-            isEnabled = true
-        }
         map.apply {
             title = "map test"
-            zoomController.setVisibility(CustomZoomButtonsController.Visibility.ALWAYS)
+            zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+            controller.setZoom(10.0)
             minZoomLevel = 10.0
             maxZoomLevel = 20.0
             setMultiTouchControls(true)
-            overlays.add(rotationGestureOverlay)
+            overlays.add(RotationGestureOverlay(map)) // enable rotation
+            overlays.add(ScaleBarOverlay(map)) // scale bar in top-left corner
         }
 
         if (intent.extras == null) {
@@ -53,66 +156,21 @@ class MapActivity : AppCompatActivity() {
 
         // async
         GlobalScope.launch {
-            val inputStream: InputStream?
-            try {
-                inputStream = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-                    // Android 9 or less
-                    FileInputStream(File(extras.get("path") as String))
-                } else {
-                    // Android 10+
-                    applicationContext.contentResolver.openInputStream(extras.get("uri") as Uri)
-                }
-            } catch (ex: Exception) {
-                Timber.wtf(ex.localizedMessage)
-                this@MapActivity.finish()
+            val line = parseFileToPolyLine(extras)
+            if (isDestroyed) {
                 return@launch
             }
-            if (inputStream == null) {
-                Timber.wtf("Failed to create inputStream for %s", extras.get("title"))
+            if (line == null) {
                 this@MapActivity.finish()
                 return@launch
             }
 
-            val reader = BufferedReader(InputStreamReader(inputStream))
-            val header = reader.readLine().split(",").toTypedArray()
-            val latIndex = header.indexOf("latitude")
-            val longIndex = header.indexOf("longitude")
-            if (latIndex == -1 || longIndex == -1) {
-                inputStream.close()
-                Timber.wtf("%s file does not contain geolocation data.", extras.get("title"))
-                this@MapActivity.finish()
-                return@launch
-            }
-            val geoPoints = ArrayList<GeoPoint>()
-            var i = 0
-            try {
-                reader.forEachLine { line ->
-                    val row = line.split(",").toTypedArray()
-                    val latitude = row[latIndex].toDouble()
-                    val longitude = row[longIndex].toDouble()
-                    geoPoints.add(i++, GeoPoint(latitude, longitude))
-                }
-            } catch (ex: Exception) {
-                Timber.wtf(ex.localizedMessage)
-                inputStream.close()
-                this@MapActivity.finish()
-                return@launch
-            }
-            inputStream.close()
-
-            val line = Polyline().apply {
-                setPoints(geoPoints)
-                outlinePaint.color = Color.BLACK // TODO: change color
-            }
-
-            if (!isDestroyed) {
-                // in UI thread
-                MainScope().launch {
-                    map.apply {
-                        isVisible = true
-                        overlays.add(line)
-                        map.zoomToBoundingBox(line.bounds, true, MathsUtil.dpToPx(context, 24))
-                    }
+            // UI thread - show map and line
+            MainScope().launch {
+                map.apply {
+                    isVisible = true
+                    overlays.add(line)
+                    zoomToBoundingBox(line.bounds, true, MathsUtil.dpToPx(context, 24))
                 }
             }
         }
