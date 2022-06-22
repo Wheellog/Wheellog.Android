ad5b42aabb875ccc0821a39dc7e7c8ba4311e16d
Pavel Yakovlev <hronos2t@gmail.com>
2022-06-22 20:54:49 +0400

    start mainActivity with ignoreBatteryOptimization

--- app/src/main/java/com/cooper/wheellog/SplashActivity.kt
+++ app/src/main/java/com/cooper/wheellog/SplashActivity.kt
@@ -6,9 +6,8 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import android.provider.Settings
+import kotlinx.coroutines.*
 
 
 class SplashActivity: Activity() {
@@ -18,6 +17,9 @@ class SplashActivity: Activity() {
         super.onCreate(savedInstanceState)
 
         val intent = Intent(this, MainActivity::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            intent.action = Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
+        }
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         val options = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             ActivityOptions.makeSceneTransitionAnimation(this).toBundle()
@@ -25,7 +27,9 @@ class SplashActivity: Activity() {
             null
         }
 
-        GlobalScope.launch {
+        val job = Job()
+        val scope = CoroutineScope(job)
+        scope.launch {
             startActivity(intent, options)
             delay(5000)
             finish()
