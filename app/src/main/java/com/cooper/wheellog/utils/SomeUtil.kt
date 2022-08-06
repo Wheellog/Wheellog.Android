db68b2933251b4d1092e8ed19563849fe60510bb
Pavel Yakovlev <hronos2t@gmail.com>
2022-08-06 20:24:27 +0400

    templates by wheel type

--- app/src/main/java/com/cooper/wheellog/utils/SomeUtil.kt
+++ app/src/main/java/com/cooper/wheellog/utils/SomeUtil.kt
@@ -155,42 +155,52 @@ class SomeUtil {
             val dialogView: View = inflater.inflate(R.layout.update_pwm_settings, null)
             val svLayout : LinearLayout = dialogView.findViewById(R.id.set_speed_voltage_layout)
             val templatesBox: Spinner = dialogView.findViewById(R.id.spinner_templates)
-            templatesBox.visibility = View.GONE
-            val templates = mutableMapOf(
-                    "Begode MTen 67v" to Pair(4400, 6720), // first - speed, second - voltage
-                    "Begode MTen 84v" to Pair(5600, 8400),
-                    "Begode MCM5 67v" to Pair(4400, 6720),
-                    "Begode MCM5v2 67v" to Pair(5120, 6720),
-                    "Begode MCM5 84v" to Pair(5600, 8400),
-                    "Begode MCM5v2 84v" to Pair(6400, 8400),
-                    "Begode Tesla/T3 84v" to Pair(6650, 8400),
-                    "Begode Nikola 84v" to Pair(7060, 8400),
-                    "Begode Nikola 100v" to Pair(8550, 10080),
-                    "Begode MSX 84v" to Pair(7900, 8400),
-                    "Begode MSX 100v" to Pair(9500, 10080),
-                    "Begode MSP HS (C30)" to Pair(10050, 10080),
-                    "Begode MSP HT (C38)" to Pair(7900, 10080),
-                    "Begode EX (C40)" to Pair(7900, 10080),
-                    "Begode EX.N (C30)" to Pair(10710, 10080),
-                    "Begode RS HS (C30)" to Pair(10500, 10080),
-                    "Begode RS HT (C38)" to Pair(7900, 10080),
-                    "Begode Hero HS (C30)" to Pair(10500, 10080),
-                    "Begode Hero HT (C38)" to Pair(7900, 10080),
-                    "Begode Master (C38)" to Pair(11300, 13440),
-                    "Begode Monster 84v" to Pair(7440, 10080),
-                    "Begode Monster 100v" to Pair(9300, 10080),
-
-                    "Veteran Sherman" to Pair(10200, 10080),
-
-                    "Ninebot Z6" to Pair(6150, 5770),
-                    "Ninebot Z8/Z10" to Pair(8150, 5770),
-
-                    "Inmotion V5F" to Pair(3700, 8400),
-                    "Inmotion V8" to Pair(4500, 8400),
-                    "Inmotion V8F/V8S" to Pair(5800, 8400),
-                    "Inmotion V10/V10F" to Pair(5500, 8400),
-
+            val templates = when (WheelData.getInstance().wheelType) {
+                Constants.WHEEL_TYPE.GOTWAY ->
+                    mutableMapOf(
+                        "Begode MTen 67v" to Pair(4400, 6720), // first - speed, second - voltage
+                        "Begode MTen 84v" to Pair(5600, 8400),
+                        "Begode MCM5 67v" to Pair(4400, 6720),
+                        "Begode MCM5v2 67v" to Pair(5120, 6720),
+                        "Begode MCM5 84v" to Pair(5600, 8400),
+                        "Begode MCM5v2 84v" to Pair(6400, 8400),
+                        "Begode Tesla/T3 84v" to Pair(6650, 8400),
+                        "Begode Nikola 84v" to Pair(7060, 8400),
+                        "Begode Nikola 100v" to Pair(8550, 10080),
+                        "Begode MSX 84v" to Pair(7900, 8400),
+                        "Begode MSX 100v" to Pair(9500, 10080),
+                        "Begode MSP HS (C30)" to Pair(10050, 10080),
+                        "Begode MSP HT (C38)" to Pair(7900, 10080),
+                        "Begode EX (C40)" to Pair(7900, 10080),
+                        "Begode EX.N (C30)" to Pair(10710, 10080),
+                        "Begode RS HS (C30)" to Pair(10500, 10080),
+                        "Begode RS HT (C38)" to Pair(7900, 10080),
+                        "Begode Hero HS (C30)" to Pair(10500, 10080),
+                        "Begode Hero HT (C38)" to Pair(7900, 10080),
+                        "Begode Master (C38)" to Pair(11300, 13440),
+                        "Begode Monster 84v" to Pair(7440, 10080),
+                        "Begode Monster 100v" to Pair(9300, 10080)
                     )
+                Constants.WHEEL_TYPE.VETERAN ->
+                    mutableMapOf(
+                        "Veteran Sherman" to Pair(10200, 10080)
+                    )
+                Constants.WHEEL_TYPE.NINEBOT_Z ->
+                    mutableMapOf(
+                        "Ninebot Z6" to Pair(6150, 5770),
+                        "Ninebot Z8/Z10" to Pair(8150, 5770)
+                    )
+                Constants.WHEEL_TYPE.INMOTION ->
+                    mutableMapOf(
+                        "Inmotion V5F" to Pair(3700, 8400),
+                        "Inmotion V8" to Pair(4500, 8400),
+                        "Inmotion V8F/V8S" to Pair(5800, 8400),
+                        "Inmotion V10/V10F" to Pair(5500, 8400)
+                    )
+                else ->
+                    mutableMapOf()
+            }
+            templatesBox.visibility = View.GONE
             templatesBox.adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1,
                 templates.toList().map { it.first })
             var selectedOption = 1
