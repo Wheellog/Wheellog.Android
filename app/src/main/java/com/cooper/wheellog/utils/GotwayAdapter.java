481e707b74f13513ed45ae6dfe639aca90a90f76
Alexander Sobyanin <me@alexandersobyanin.ru>
2023-09-22 17:42:16 +0500

    Remove excessive condition for gotway custom firmware light mode switch

--- app/src/main/java/com/cooper/wheellog/utils/GotwayAdapter.java
+++ app/src/main/java/com/cooper/wheellog/utils/GotwayAdapter.java
@@ -240,8 +240,9 @@ public class GotwayAdapter extends BaseAdapter {
         }
         // Strobe light not available on Freestyl3r firmware while pwm tiltback mode enabled.
         // For custom firmware with enabled tiltback available only light off and on.
-        // Strobe is using for tiltback warning. Detect this by Hw PWM option and specific alarm mode.
-        if (WheelLog.AppConfig.getHwPwm() && WheelLog.AppConfig.getAlarmMode().equals(String.valueOf(alarmModeCF)) && lightMode > lightModeOn) {
+        // Strobe is using for tiltback warning. Detect via specific for this firmware alarm mode.
+        if (lightMode > lightModeOn
+                && WheelLog.AppConfig.getAlarmMode().equals(String.valueOf(alarmModeCF))) {
             lightMode = lightModeOff;
         }
         WheelLog.AppConfig.setLightMode(String.valueOf(lightMode));
