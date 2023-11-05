4a29cb44356c0ccd707be500af7caad5e38f3376
Nathan Retta <nretta@gmail.com>
2023-11-05 09:20:52 -0700

    Extract different frame decoders into private methods

--- app/src/main/java/com/cooper/wheellog/utils/kingsong/KingsongAdapter.java
+++ app/src/main/java/com/cooper/wheellog/utils/kingsong/KingsongAdapter.java
@@ -1,4 +1,5 @@
 package com.cooper.wheellog.utils.kingsong;
+
 import static com.cooper.wheellog.utils.kingsong.KingsongUtils.is100vWheel;
 import static com.cooper.wheellog.utils.kingsong.KingsongUtils.is126vWheel;
 import static com.cooper.wheellog.utils.kingsong.KingsongUtils.is84vWheel;
@@ -9,7 +10,6 @@ import com.cooper.wheellog.WheelLog;
 import com.cooper.wheellog.utils.BaseAdapter;
 import com.cooper.wheellog.utils.MathsUtil;
 import com.cooper.wheellog.utils.SmartBms;
-import com.cooper.wheellog.utils.StringUtil;
 
 import java.util.Locale;
 import java.util.Objects;
@@ -57,175 +57,210 @@ public class KingsongAdapter extends BaseAdapter {
                 mMode = result.mode();
                 return true;
             } else if ((data[16] & 255) == 0xB9) { // Distance/Time/Fan Data
-                long distance = MathsUtil.getInt4R(data, 2);
-                wd.setWheelDistance(distance);
-                wd.updateRideTime();
-                wd.setTopSpeed(MathsUtil.getInt2R(data, 8));
-                wd.setFanStatus(data[12]);
-                wd.setChargingStatus(data[13]);
-                wd.setTemperature2(MathsUtil.getInt2R(data, 14));
+                decodeKingsongDistanceTimeFan(data);
                 return false;
             } else if ((data[16] & 255) == 187) { // Name and Type data
-                int end = 0;
-                int i = 0;
-                while (i < 14 && data[i + 2] != 0) {
-                    end++;
-                    i++;
-                }
-                wd.setName(new String(data, 2, end).trim());
-                wd.setModel("");
-                String[] ss = wd.getName().split("-");
-                StringBuilder model = new StringBuilder();
-                for (i = 0; i < ss.length - 1; i++) {
-                    if (i != 0) {
-                        model.append("-");
-                    }
-                    model.append(ss[i]);
-                }
-                wd.setModel(model.toString());
-                try {
-                    wd.setVersion(String.format(Locale.US, "%.2f", Integer.parseInt(ss[ss.length - 1]) / 100.0));
-                } catch (Exception ignored) {
-                }
+                decodeKingsongNameAndTypeData(data);
                 return false;
             } else if ((data[16] & 255) == 0xB3) { // Serial Number
-                byte[] sndata = new byte[18];
-                System.arraycopy(data, 2, sndata, 0, 14);
-                System.arraycopy(data, 17, sndata, 14, 3);
-                sndata[17] = (byte) 0;
-                wd.setSerial(new String(sndata));
-                updateKSAlarmAndSpeed();
+                decodeKingsongSerialNumber(data);
                 return false;
             } else if ((data[16] & 255) == 0xF5) { //cpu load
-                wd.setCpuLoad(data[14]);
-                wd.setOutput(data[15]*100);
+                decodeKingsongCpuLoad(data);
                 return false;
             } else if ((data[16] & 255) == 0xF6) { //speed limit (PWM?)
-                mSpeedLimit = MathsUtil.getInt2R(data, 2) / 100.0;
-                wd.setSpeedLimit(mSpeedLimit);
+                decodeKingsongSpeed(data);
                 return false;
             } else if ((data[16] & 255) == 0xA4 || (data[16] & 255) == 0xB5) { //max speed and alerts
-                mWheelMaxSpeed = data[10] & 255;
-                appConfig.setWheelMaxSpeed(mWheelMaxSpeed);
-                mKSAlarm3Speed = (data[8] & 255);
-                mKSAlarm2Speed = (data[6] & 255);
-                mKSAlarm1Speed = (data[4] & 255);
-                appConfig.setWheelKsAlarm3(mKSAlarm3Speed);
-                appConfig.setWheelKsAlarm2(mKSAlarm2Speed);
-                appConfig.setWheelKsAlarm1(mKSAlarm1Speed);
-                // after received 0xa4 send same repeat data[2] =0x01 data[16] = 0x98
-                if ((data[16] & 255) == 164) {
-                    data[16] = (byte) 0x98;
-                    wd.bluetoothCmd(data);
-                }
+                deocdeKingsongMaxSpeedAndAlerts(data);
                 return true;
             } else if ((data[16] & 255) == 0xF1 || (data[16] & 255) == 0xF2) { // F1 - 1st BMS, F2 - 2nd BMS. F3 and F4 are also present but empty
-                int bmsnum = (data[16] & 255) - 0xF0;
-                SmartBms bms = bmsnum == 1 ? wd.getBms1() : wd.getBms2();
-                int pNum = (data[17] & 255);
-                if (pNum == 0x00) {
-                    bms.setVoltage(MathsUtil.getInt2R(data, 2)/100.0);
-                    bms.setCurrent(MathsUtil.getInt2R(data, 4)/100.0);
-                    bms.setRemCap(MathsUtil.getInt2R(data, 6)*10);
-                    bms.setFactoryCap(MathsUtil.getInt2R(data, 8)*10);
-                    bms.setFullCycles(MathsUtil.getInt2R(data, 10));
-                    bms.setRemPerc((int)Math.round(bms.getRemCap()/(bms.getFactoryCap()/100.0)));
-                    if (bms.getSerialNumber().equals("")) {
-                        if (bmsnum == 1) {
-                            requestBms1Serial();
-                        } else {
-                            requestBms2Serial();
-                        }
-                    }
-                } else if (pNum == 0x01) {
-                    bms.setTemp1((MathsUtil.getInt2R(data, 2)-2730)/10.0);
-                    bms.setTemp2((MathsUtil.getInt2R(data, 4)-2730)/10.0);
-                    bms.setTemp3((MathsUtil.getInt2R(data, 6)-2730)/10.0);
-                    bms.setTemp4((MathsUtil.getInt2R(data, 8)-2730)/10.0);
-                    bms.setTemp5((MathsUtil.getInt2R(data, 10)-2730)/10.0);
-                    bms.setTemp6((MathsUtil.getInt2R(data, 12)-2730)/10.0);
-                    bms.setTempMos((MathsUtil.getInt2R(data, 14)-2730)/10.0);
-                } else if (pNum == 0x02) {
-                    bms.getCells()[0] = MathsUtil.getInt2R(data, 2)/1000.0;
-                    bms.getCells()[1] = MathsUtil.getInt2R(data, 4)/1000.0;
-                    bms.getCells()[2] = MathsUtil.getInt2R(data, 6)/1000.0;
-                    bms.getCells()[3] = MathsUtil.getInt2R(data, 8)/1000.0;
-                    bms.getCells()[4] = MathsUtil.getInt2R(data, 10)/1000.0;
-                    bms.getCells()[5] = MathsUtil.getInt2R(data, 12)/1000.0;
-                    bms.getCells()[6] = MathsUtil.getInt2R(data, 14)/1000.0;
-                } else if (pNum == 0x03) {
-                    bms.getCells()[7] = MathsUtil.getInt2R(data, 2)/1000.0;
-                    bms.getCells()[8] = MathsUtil.getInt2R(data, 4)/1000.0;
-                    bms.getCells()[9] = MathsUtil.getInt2R(data, 6)/1000.0;
-                    bms.getCells()[10] = MathsUtil.getInt2R(data, 8)/1000.0;
-                    bms.getCells()[11] = MathsUtil.getInt2R(data, 10)/1000.0;
-                    bms.getCells()[12] = MathsUtil.getInt2R(data, 12)/1000.0;
-                    bms.getCells()[13] = MathsUtil.getInt2R(data, 14)/1000.0;
-                } else if (pNum == 0x04) {
-                    bms.getCells()[14] = MathsUtil.getInt2R(data, 2)/1000.0;
-                    bms.getCells()[15] = MathsUtil.getInt2R(data, 4)/1000.0;
-                    bms.getCells()[16] = MathsUtil.getInt2R(data, 6)/1000.0;
-                    bms.getCells()[17] = MathsUtil.getInt2R(data, 8)/1000.0;
-                    bms.getCells()[18] = MathsUtil.getInt2R(data, 10)/1000.0;
-                    bms.getCells()[19] = MathsUtil.getInt2R(data, 12)/1000.0;
-                    bms.getCells()[20] = MathsUtil.getInt2R(data, 14)/1000.0;
-                } else if (pNum == 0x05) {
-                    bms.getCells()[21] = MathsUtil.getInt2R(data, 2)/1000.0;
-                    bms.getCells()[22] = MathsUtil.getInt2R(data, 4)/1000.0;
-                    bms.getCells()[23] = MathsUtil.getInt2R(data, 6)/1000.0;
-                    bms.getCells()[24] = MathsUtil.getInt2R(data, 8)/1000.0;
-                    bms.getCells()[25] = MathsUtil.getInt2R(data, 10)/1000.0;
-                    bms.getCells()[26] = MathsUtil.getInt2R(data, 12)/1000.0;
-                    bms.getCells()[27] = MathsUtil.getInt2R(data, 14)/1000.0;
-                } else if (pNum == 0x06) {
-                    bms.getCells()[28] = MathsUtil.getInt2R(data, 2)/1000.0;
-                    bms.getCells()[29] = MathsUtil.getInt2R(data, 4)/1000.0;
-                    //bms.getCells()[30] = MathsUtil.getInt2R(data, 6)/1000.0;
-                    //bms.getCells()[31] = MathsUtil.getInt2R(data, 8)/1000.0;
-                    bms.setTempMosEnv((MathsUtil.getInt2R(data, 10)-2730)/10.0);
-                    //bms.getCells()[5] = MathsUtil.getInt2R(data, 12)/1000.0;
-                    bms.setMinCell(bms.getCells()[29]);
-                    bms.setMaxCell(bms.getCells()[29]);
-                    for (int i = 0; i < 30; i++) {
-                        double cell = bms.getCells()[i];
-                        if (cell > 0.0) {
-                            if (bms.getMaxCell() < cell) {
-                                bms.setMaxCell(cell);
-                            }
-                            if (bms.getMinCell() > cell) {
-                                bms.setMinCell(cell);
-                            }
-                        }
-                    }
-                    bms.setCellDiff(bms.getMaxCell() - bms.getMinCell());
-                    if (bms.getVersionNumber().equals("")) {
-                        if (bmsnum == 1) {
-                            requestBms1Firmware();
-                        } else {
-                            requestBms2Firmware();
-                        }
-                    }
-                }
+                decodeFFrames(data);
             } else if ((data[16] & 255) == 0xe1 || (data[16] & 255) == 0xe2) { // e1 - 1st BMS, e2 - 2nd BMS.
-                int bmsnum = (data[16] & 255) - 0xE0;
-                SmartBms bms = bmsnum == 1 ? wd.getBms1() : wd.getBms2();
-                byte[] sndata = new byte[18];
-                System.arraycopy(data, 2, sndata, 0, 14);
-                System.arraycopy(data, 17, sndata, 14, 3);
-                sndata[17] = (byte) 0;
-                bms.setSerialNumber(new String(sndata));
+                decodeE1E2Frames(data);
             } else if ((data[16] & 255) == 0xe5 || (data[16] & 255) == 0xe6) { // e5 - 1st BMS, e6 - 2nd BMS.
-                int bmsnum = (data[16] & 255) - 0xE4;
-                SmartBms bms = bmsnum == 1 ? wd.getBms1() : wd.getBms2();
-                byte[] sndata = new byte[19];
-                System.arraycopy(data, 2, sndata, 0, 14);
-                System.arraycopy(data, 17, sndata, 14, 3);
-                sndata[18] = (byte) 0;
-                bms.setVersionNumber(new String(sndata));
+                deocdE5E6Frames(data);
             }
-
         }
         return false;
+    }
+
+    private void deocdE5E6Frames(byte[] data) {
+        int bmsnum = (data[16] & 255) - 0xE4;
+        SmartBms bms = bmsnum == 1 ? wd.getBms1() : wd.getBms2();
+        byte[] sndata = new byte[19];
+        System.arraycopy(data, 2, sndata, 0, 14);
+        System.arraycopy(data, 17, sndata, 14, 3);
+        sndata[18] = (byte) 0;
+        bms.setVersionNumber(new String(sndata));
+    }
+
+    private void decodeE1E2Frames(byte[] data) {
+        int bmsnum = (data[16] & 255) - 0xE0;
+        SmartBms bms = bmsnum == 1 ? wd.getBms1() : wd.getBms2();
+        byte[] sndata = new byte[18];
+        System.arraycopy(data, 2, sndata, 0, 14);
+        System.arraycopy(data, 17, sndata, 14, 3);
+        sndata[17] = (byte) 0;
+        bms.setSerialNumber(new String(sndata));
+    }
+
+    private void decodeFFrames(byte[] data) {
+        int bmsnum = (data[16] & 255) - 0xF0;
+        SmartBms bms = bmsnum == 1 ? wd.getBms1() : wd.getBms2();
+        int pNum = (data[17] & 255);
+        if (pNum == 0x00) {
+            bms.setVoltage(MathsUtil.getInt2R(data, 2) / 100.0);
+            bms.setCurrent(MathsUtil.getInt2R(data, 4) / 100.0);
+            bms.setRemCap(MathsUtil.getInt2R(data, 6) * 10);
+            bms.setFactoryCap(MathsUtil.getInt2R(data, 8) * 10);
+            bms.setFullCycles(MathsUtil.getInt2R(data, 10));
+            bms.setRemPerc((int) Math.round(bms.getRemCap() / (bms.getFactoryCap() / 100.0)));
+            if (bms.getSerialNumber().equals("")) {
+                if (bmsnum == 1) {
+                    requestBms1Serial();
+                } else {
+                    requestBms2Serial();
+                }
+            }
+        } else if (pNum == 0x01) {
+            bms.setTemp1((MathsUtil.getInt2R(data, 2) - 2730) / 10.0);
+            bms.setTemp2((MathsUtil.getInt2R(data, 4) - 2730) / 10.0);
+            bms.setTemp3((MathsUtil.getInt2R(data, 6) - 2730) / 10.0);
+            bms.setTemp4((MathsUtil.getInt2R(data, 8) - 2730) / 10.0);
+            bms.setTemp5((MathsUtil.getInt2R(data, 10) - 2730) / 10.0);
+            bms.setTemp6((MathsUtil.getInt2R(data, 12) - 2730) / 10.0);
+            bms.setTempMos((MathsUtil.getInt2R(data, 14) - 2730) / 10.0);
+        } else if (pNum == 0x02) {
+            bms.getCells()[0] = MathsUtil.getInt2R(data, 2) / 1000.0;
+            bms.getCells()[1] = MathsUtil.getInt2R(data, 4) / 1000.0;
+            bms.getCells()[2] = MathsUtil.getInt2R(data, 6) / 1000.0;
+            bms.getCells()[3] = MathsUtil.getInt2R(data, 8) / 1000.0;
+            bms.getCells()[4] = MathsUtil.getInt2R(data, 10) / 1000.0;
+            bms.getCells()[5] = MathsUtil.getInt2R(data, 12) / 1000.0;
+            bms.getCells()[6] = MathsUtil.getInt2R(data, 14) / 1000.0;
+        } else if (pNum == 0x03) {
+            bms.getCells()[7] = MathsUtil.getInt2R(data, 2) / 1000.0;
+            bms.getCells()[8] = MathsUtil.getInt2R(data, 4) / 1000.0;
+            bms.getCells()[9] = MathsUtil.getInt2R(data, 6) / 1000.0;
+            bms.getCells()[10] = MathsUtil.getInt2R(data, 8) / 1000.0;
+            bms.getCells()[11] = MathsUtil.getInt2R(data, 10) / 1000.0;
+            bms.getCells()[12] = MathsUtil.getInt2R(data, 12) / 1000.0;
+            bms.getCells()[13] = MathsUtil.getInt2R(data, 14) / 1000.0;
+        } else if (pNum == 0x04) {
+            bms.getCells()[14] = MathsUtil.getInt2R(data, 2) / 1000.0;
+            bms.getCells()[15] = MathsUtil.getInt2R(data, 4) / 1000.0;
+            bms.getCells()[16] = MathsUtil.getInt2R(data, 6) / 1000.0;
+            bms.getCells()[17] = MathsUtil.getInt2R(data, 8) / 1000.0;
+            bms.getCells()[18] = MathsUtil.getInt2R(data, 10) / 1000.0;
+            bms.getCells()[19] = MathsUtil.getInt2R(data, 12) / 1000.0;
+            bms.getCells()[20] = MathsUtil.getInt2R(data, 14) / 1000.0;
+        } else if (pNum == 0x05) {
+            bms.getCells()[21] = MathsUtil.getInt2R(data, 2) / 1000.0;
+            bms.getCells()[22] = MathsUtil.getInt2R(data, 4) / 1000.0;
+            bms.getCells()[23] = MathsUtil.getInt2R(data, 6) / 1000.0;
+            bms.getCells()[24] = MathsUtil.getInt2R(data, 8) / 1000.0;
+            bms.getCells()[25] = MathsUtil.getInt2R(data, 10) / 1000.0;
+            bms.getCells()[26] = MathsUtil.getInt2R(data, 12) / 1000.0;
+            bms.getCells()[27] = MathsUtil.getInt2R(data, 14) / 1000.0;
+        } else if (pNum == 0x06) {
+            bms.getCells()[28] = MathsUtil.getInt2R(data, 2) / 1000.0;
+            bms.getCells()[29] = MathsUtil.getInt2R(data, 4) / 1000.0;
+            //bms.getCells()[30] = MathsUtil.getInt2R(data, 6)/1000.0;
+            //bms.getCells()[31] = MathsUtil.getInt2R(data, 8)/1000.0;
+            bms.setTempMosEnv((MathsUtil.getInt2R(data, 10) - 2730) / 10.0);
+            //bms.getCells()[5] = MathsUtil.getInt2R(data, 12)/1000.0;
+            bms.setMinCell(bms.getCells()[29]);
+            bms.setMaxCell(bms.getCells()[29]);
+            for (int i = 0; i < 30; i++) {
+                double cell = bms.getCells()[i];
+                if (cell > 0.0) {
+                    if (bms.getMaxCell() < cell) {
+                        bms.setMaxCell(cell);
+                    }
+                    if (bms.getMinCell() > cell) {
+                        bms.setMinCell(cell);
+                    }
+                }
+            }
+            bms.setCellDiff(bms.getMaxCell() - bms.getMinCell());
+            if (bms.getVersionNumber().equals("")) {
+                if (bmsnum == 1) {
+                    requestBms1Firmware();
+                } else {
+                    requestBms2Firmware();
+                }
+            }
+        }
+    }
+
+    private void deocdeKingsongMaxSpeedAndAlerts(byte[] data) {
+        mWheelMaxSpeed = data[10] & 255;
+        appConfig.setWheelMaxSpeed(mWheelMaxSpeed);
+        mKSAlarm3Speed = (data[8] & 255);
+        mKSAlarm2Speed = (data[6] & 255);
+        mKSAlarm1Speed = (data[4] & 255);
+        appConfig.setWheelKsAlarm3(mKSAlarm3Speed);
+        appConfig.setWheelKsAlarm2(mKSAlarm2Speed);
+        appConfig.setWheelKsAlarm1(mKSAlarm1Speed);
+        // after received 0xa4 send same repeat data[2] =0x01 data[16] = 0x98
+        if ((data[16] & 255) == 164) {
+            data[16] = (byte) 0x98;
+            wd.bluetoothCmd(data);
+        }
+    }
+
+    private void decodeKingsongSpeed(byte[] data) {
+        mSpeedLimit = MathsUtil.getInt2R(data, 2) / 100.0;
+        wd.setSpeedLimit(mSpeedLimit);
+    }
+
+    private void decodeKingsongCpuLoad(byte[] data) {
+        wd.setCpuLoad(data[14]);
+        wd.setOutput(data[15] * 100);
+    }
+
+    private void decodeKingsongSerialNumber(byte[] data) {
+        byte[] sndata = new byte[18];
+        System.arraycopy(data, 2, sndata, 0, 14);
+        System.arraycopy(data, 17, sndata, 14, 3);
+        sndata[17] = (byte) 0;
+        wd.setSerial(new String(sndata));
+        updateKSAlarmAndSpeed();
+    }
+
+    private void decodeKingsongNameAndTypeData(byte[] data) {
+        int end = 0;
+        int i = 0;
+        while (i < 14 && data[i + 2] != 0) {
+            end++;
+            i++;
+        }
+        wd.setName(new String(data, 2, end).trim());
+        wd.setModel("");
+        String[] ss = wd.getName().split("-");
+        StringBuilder model = new StringBuilder();
+        for (i = 0; i < ss.length - 1; i++) {
+            if (i != 0) {
+                model.append("-");
+            }
+            model.append(ss[i]);
+        }
+        wd.setModel(model.toString());
+        try {
+            wd.setVersion(String.format(Locale.US, "%.2f", Integer.parseInt(ss[ss.length - 1]) / 100.0));
+        } catch (Exception ignored) {
+        }
+    }
+
+    private void decodeKingsongDistanceTimeFan(byte[] data) {
+        long distance = MathsUtil.getInt4R(data, 2);
+        wd.setWheelDistance(distance);
+        wd.updateRideTime();
+        wd.setTopSpeed(MathsUtil.getInt2R(data, 8));
+        wd.setFanStatus(data[12]);
+        wd.setChargingStatus(data[13]);
+        wd.setTemperature2(MathsUtil.getInt2R(data, 14));
     }
 
     @Override
@@ -274,9 +309,13 @@ public class KingsongAdapter extends BaseAdapter {
     @Override
     public int getCellsForWheel() {
         int cells = 16;
-        if (is84vWheel(wd)) {cells = 20; }
-        else if (is126vWheel(wd)) {cells = 30; }
-        else if (is100vWheel(wd)) { cells = 24; }
+        if (is84vWheel(wd)) {
+            cells = 20;
+        } else if (is126vWheel(wd)) {
+            cells = 30;
+        } else if (is100vWheel(wd)) {
+            cells = 24;
+        }
         return cells;
     }
 
